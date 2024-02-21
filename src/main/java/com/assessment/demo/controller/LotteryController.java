12d893f5a2875613c11e73b5a7e20f3d064d9a36
Bumbimism <aqualiux.nittha@gmail.com>
2024-02-21 18:12:27 +0700

    edit getLotteries Method

--- src/main/java/com/assessment/demo/controller/LotteryController.java
+++ src/main/java/com/assessment/demo/controller/LotteryController.java
@@ -2,17 +2,13 @@ package com.assessment.demo.controller;
 
 import com.assessment.demo.entity.Lottery;
 import com.assessment.demo.entity.UserTicket;
-import com.assessment.demo.response.ResponseHandler;
 import com.assessment.demo.service.IdResponse;
 import com.assessment.demo.service.LotteryApiService;
 import com.assessment.demo.service.LotteryResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping()
