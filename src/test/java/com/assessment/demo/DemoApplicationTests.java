3f6124af85e5791c7dfd778cd0ee548d04415145
Bumbimism <aqualiux.nittha@gmail.com>
2024-02-24 18:30:04 +0700

    100% test coverage in controller layer

--- src/test/java/com/assessment/demo/DemoApplicationTests.java
+++ src/test/java/com/assessment/demo/DemoApplicationTests.java
@@ -2,17 +2,18 @@ package com.assessment.demo;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
+@TestConfiguration(proxyBeanMethods = false)
 @SpringBootTest
 @AutoConfigureMockMvc
 class DemoApplicationTests{
 
-	@Test
-	void contextLoads() {
-		assert true;}
-
+	public static void main(String[] args) {
+		SpringApplication.from(DemoApplication::main).with(DemoApplication.class).run(args);
+	}
 }
--- src/test/java/com/assessment/demo/controller/AdminControllerTest.java
+++ src/test/java/com/assessment/demo/controller/AdminControllerTest.java
@@ -3,16 +3,16 @@ package com.assessment.demo.controller;
 import com.assessment.demo.request.LotteryRequest;
 import com.assessment.demo.response.LotteryResponse;
 import com.assessment.demo.service.LotteryApiService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import java.util.HashMap;
 
 import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.when;
@@ -41,14 +41,11 @@ public class AdminControllerTest {
     @DisplayName("when post /lotteries then return ticket list")
     void createLottery() throws Exception {
         LotteryRequest lotteryRequest = new LotteryRequest("888888", 80, 1);
-        HashMap<String, Object> jsonObject = new HashMap<String, Object>();
-        jsonObject.put("ticket", "Hello from map");
-        jsonObject.put("price", 80);
-        jsonObject.put("amount", 1);
+
         when(lotteryApiService.createLottery(lotteryRequest)).thenReturn(new LotteryResponse("888888"));
 
-        mockMvc.perform(post("/admin/lotteries").contentType("application/json")
-                        .content(String.valueOf(jsonObject)))
+        mockMvc.perform(post("/admin/lotteries").contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(lotteryRequest)))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.ticket", is("888888")));
     }
