9e963f252e3d6580c68c1c80e153470d7cd62823
Bumbimism <aqualiux.nittha@gmail.com>
2024-02-26 21:20:03 +0700

    remove unused import

--- src/test/java/com/assessment/demo/DemoApplicationTests.java
+++ src/test/java/com/assessment/demo/DemoApplicationTests.java
@@ -1,19 +1,16 @@
 package com.assessment.demo;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @TestConfiguration(proxyBeanMethods = false)
 @SpringBootTest
 @AutoConfigureMockMvc
-class DemoApplicationTests{
+class DemoApplicationTests {
 
-	public static void main(String[] args) {
-		SpringApplication.from(DemoApplication::main).with(DemoApplication.class).run(args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.from(DemoApplication::main).with(DemoApplication.class).run(args);
+    }
 }
